168214750c6114d8c0d9c73c80b64be113372030
KrishanMihiranga <xp3cto95@gmail.com>
2024-06-19 02:46:08 +0530

    feat: Configure MonogDB, Implement fetch posts to Homepage,card structure,save reply comment to DB

--- threads/components/forms/AccountProfile.tsx
+++ threads/components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updatUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -36,7 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
-
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -77,7 +80,21 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                 values.profile_photo = imgRes[0].url;
             }
         }
-        //TODO : Update user rofile
+
+        await updatUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if (pathname === '/profile/edit') {
+            router.back();
+        }else{
+            router.push('/');
+        }
     }
 
     return (
@@ -117,6 +134,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                             {/* <FormDescription>
                                 This is your public display name.
                             </FormDescription> */}
+                            <FormMessage />
                             <FormMessage />
                         </FormItem>
                     )}
