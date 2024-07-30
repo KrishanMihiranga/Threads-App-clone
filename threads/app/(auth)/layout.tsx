dc61d3e3611f31a213dc719ee8ca1a3464b02076
KrishanMihiranga <xp3cto95@gmail.com>
2024-07-30 19:26:00 +0530

    feat: Implement activity page

--- threads/app/(auth)/layout.tsx
+++ threads/app/(auth)/layout.tsx
@@ -7,14 +7,16 @@ export const metadata = {
     description: 'A next.js 14 Meta Threads Application clone'
 }
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
-                    {children}
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
                 </body>
 
             </html>
